098569b1893d7928937b60c13816bd67d717d947
Daniel Arbelo Arrocha <arbelo@gmail.com>
2009-07-17 22:30:59 +0000

    Push the PCT decTest parser forward, it now emits PIR that looks like it might be correct, despite the fact that it's not runnable yet.

--- aux/decTest/src/pct/actions.pm
+++ aux/decTest/src/pct/actions.pm
@@ -24,6 +24,50 @@ method TOP($/) {
     make $past;
 }
 
+method statement($/, $key) {
+    make $/{$key}.ast;
+}
+
+method test($/) {
+    my $past := PAST::Block.new( :blocktype('declaration'), :node( $/ ),
+                                 :name($<testname>), :hll('decTest') );
+    my $operation := PAST::Op.new( :name($<operation>), :node( $/ ),
+                                   :pasttype('call') );
+    for $<number> {
+        $operation.push( $_.ast );
+    }
+    my $cmp := PAST::Op.new( :name('is'), :pasttype('call'),
+                             :node( $/ ) );
+    $cmp.push( $operation.pop() );
+    $cmp.push( $operation );
+    $past.push( $cmp );
+    make $past;
+}
+
+method number($/) {
+    make PAST::Val.new( :value( ~$/ ), :returns('DecNum'), :node($/) );
+}
+
+method context($/) {
+    my $past := PAST::Op.new(:node( $/ ), :pasttype('callmethod'), :name( $<context_field> ) );
+    $past.push(PAST::Val.new( :returns('DecNumContext'), :value( 'FIXME' ) ));
+    if ( $<integer> ) {
+        $past.push( $<integer>.ast );
+    }
+    if ( $<rounding_mode> ) {
+        $past.push( $<rounding_mode>.ast );
+    }
+    make $past;
+}
+
+method integer($/) {
+    make PAST::Val.new( :value( ~$/ ), :returns('Integer'), :node($/) );
+}
+
+method rounding_mode($/) {
+    make PAST::Val.new( :value( ~$/ ), :returns('Integer'), :node($/) );
+}
+
 # Local Variables:
 #   mode: cperl
 #   cperl-indent-level: 4
